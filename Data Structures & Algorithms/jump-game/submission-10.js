0cd47cb9a7f62b0f668e4d5f11fda65ee4016a52
Kyo8300 <kametyou777@gmail.com>
2026-04-08 21:00:05 +0800

    Add: jump-game - submission-10

---
+++ Data Structures & Algorithms/jump-game/submission-10.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    canJump(nums) {
+        const memo = new Array(nums.length).fill(-1);
+        memo[nums.length - 1] = true;
+        for(let i = nums.length - 2; i >= 0; i--) {
+            for(let j = 1; j <= nums[i]; j++) {
+                if(memo[j + i] === true) {
+                    memo[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return memo[0] === true
+    }
+}
+
+// [3, 2, 5, 3, 2]
