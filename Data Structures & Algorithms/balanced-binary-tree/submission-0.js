36f091945cf0930be55ed924f7998cdc3d8b266e
Kyo8300 <kametyou777@gmail.com>
2026-04-09 22:19:46 +0800

    Add: balanced-binary-tree - submission-0

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-0.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isBalanced(root) {
+        let checkBalanced = true;
+        function dfs(node) {
+            if(!node) return 0;
+
+            const left = dfs(node.left)
+            const right = dfs(node.right)
+
+            if(Math.abs(left - right) > 1) {
+                checkBalanced = false
+            }
+
+            return Math.max(left + 1, right + 1);
+        }
+
+        dfs(root)
+
+        return checkBalanced;
+    }
+}
