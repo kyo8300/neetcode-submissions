818a4224d044cabb683aeaf8f9cc374adff9926c
Kyo8300 <kametyou777@gmail.com>
2026-05-13 20:31:51 +0900

    Add: climbing-stairs - submission-10

---
+++ Data Structures & Algorithms/climbing-stairs/submission-10.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+        if(n <= 2) return n;
+        const memo = new Array(n + 1).fill(-1);
+        memo[0] = 1;
+        memo[1] = 1;
+        for(let i = 2; i <= n; i++) {
+            memo[i] = memo[i - 1] + memo[i - 2];
+        }
+
+        return memo[n];
+    }
+}
