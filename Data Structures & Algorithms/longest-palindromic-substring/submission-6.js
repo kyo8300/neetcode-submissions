34bbaeb1a33f8ac1e8b50cd878b5c51927cd1baa
Kyo8300 <kametyou777@gmail.com>
2026-05-20 18:47:46 +0900

    Add: longest-palindromic-substring - submission-6

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-6.js
@@ -0,0 +1,54 @@
+class Solution {
+    checkPalindrome(s, l, r) {
+        let left = l;
+        let right = r;
+        while(left <= right) {
+            if(s[left] !== s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+    /**
+     * @param {string} s
+     * @return {string}
+     */
+    longestPalindrome(s) {
+        if(s.length === 1) return s;
+        let resIdx = 0;
+        let resLen = 0;
+
+        for(let i = 0; i < s.length; i++) {
+            let l = i;
+            let r = i;
+            while(l >= 0 && r < s.length && s[l] === s[r]) {
+                if(resLen < r - l + 1) {
+                    resLen = r - l + 1;
+                    resIdx = l;
+                }
+                l--;
+                r++
+            }
+
+            l = i;
+            r = i + 1;
+            while(l >= 0 && r < s.length && s[l] === s[r]) {
+                if(resLen < r - l + 1) {
+                    resLen = r - l + 1;
+                    resIdx = l;
+                }
+                l--;
+                r++
+            }
+        }
+
+        return s.slice(resIdx, resLen + resIdx);
+
+    }
+}
+
+
+
