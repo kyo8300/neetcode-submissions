fbd9b400d3445f7fd362f4701f4f82af7e202355
Kyo8300 <kametyou777@gmail.com>
2026-07-08 22:08:53 +0900

    Add: partition-equal-subset-sum - submission-3

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-3.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    canPartition(nums) {
+        const sum = nums.reduce((acc, cur) => acc + cur, 0);
+        if(sum % 2 !== 0) return false;
+        const target = Math.floor(sum / 2);
+
+        const dp = new Array(target + 1).fill(false);
+        dp[0] = true;
+        for(const num of nums) {
+            for(let i = target; i >= num; i--) {
+                dp[i] = dp[i] || dp[i - num];
+            }
+        }
+
+        return dp[target]
+    }
+}
